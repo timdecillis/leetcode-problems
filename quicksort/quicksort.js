7feba19dadd23191ad89be369cb2917db30a50a7
Tim DeCillis <timdecillis@gmail.com>
2023-05-31 20:02:43 -0700

    submit code for toy problem quicksort

--- quicksort/quicksort.js
+++ quicksort/quicksort.js
@@ -12,4 +12,21 @@
 
 
 var quicksort = function(array) {
+  var sorted = [];
+  var lower = [];
+  var higher = [];
+  var pivot = array[Math.floor(arr.length/2)];
+  array.forEach(num => {
+      if (num === pivot) {
+          return;
+      } else if (num > pivot) {
+          higher.push(num);
+      } else if (num < pivot) {
+          lower.push(num);
+      }
+  })
+
+  var concat = lower.concat(pivot);
+  return concat.concat(higher);
+
 };
