16d93c57419fbd672c07ac0892b9ffca5a9b8940
Tim DeCillis <timdecillis@gmail.com>
2023-06-05 19:38:46 -0700

    submit code for toy problem firstNonRepeatedCharacter.js

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -7,5 +7,28 @@
  */
 
 var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+  var counter = {};
+  for (var i = 0; i < string.length; i ++) {
+    var letter = string[i];
+    if (counter[letter] === undefined) {
+      counter[letter] = 1;
+    } else {
+      counter[letter] ++;
+    }
+  }
+  var nonRepeats = [];
+  var results = [];
+  for (var key in counter) {
+    if (counter[key] === 1) {
+      nonRepeats.push(key);
+    }
+  }
+  for (var j = 0; j < string.length; j ++) {
+    for (var k = 0; k < nonRepeats.length; k ++) {
+      if (string[j] === nonRepeats[k]) {
+        results.push(string[j]);
+      }
+    }
+  }
+  return results[0];
 };
