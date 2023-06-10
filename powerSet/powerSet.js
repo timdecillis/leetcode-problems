378cc0fefde0840e556aa2512958b76c2eef3acd
Tim DeCillis <timdecillis@gmail.com>
2023-06-09 20:02:12 -0700

    submit code for toy problem powerSet.js

--- powerSet/powerSet.js
+++ powerSet/powerSet.js
@@ -7,15 +7,30 @@
  * powerSet("abc")
  * -> [ '' , 'a', 'b', 'c', 'ab', 'ac', 'bc', 'abc' ]
  *
- * Note: 
+ * Note:
  *  1. All characters in a subset should be sorted.
- *  2. Sets of the same characters are considered duplicates regardless of order and count only once, e.g. 'ab' and 'ba' are the same. 
- * 
+ *  2. Sets of the same characters are considered duplicates regardless of order and count only once, e.g. 'ab' and 'ba' are the same.
+ *
  * Example 2 :
- * 
+ *
  * powerSet("jump")
  * -> ["", "j", "ju", "jm", "jp", "jmu", "jmp", "jpu", "jmpu", "u", "m", "p", "mu", "mp", "pu", "mpu"]
  */
 
-var powerSet = function(str) {
+var powerSet = function(string) {
+  var result = [''];
+  while (string !== '') {
+    if (!result.includes(string)) {
+      result.push(string);
+    }
+    for (var i = 0; i < string.length; i ++) {
+      if (!result.includes(string[i])) {
+        result.push(string[i]);
+      }
+    }
+
+    string = string.slice(1);
+  }
+  return result;
+
 };
